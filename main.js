7fd379a49d89799bb32f03296dfe77517694608c
ThanhDang <xuanthanh.110699@gmail.com>
2022-03-07 15:48:28 +0700

    up

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const headerMenu = document.querySelector("header");
-const brandLogo = document.querySelector(".brand-logo")
+const brandLogo = document.querySelector(".brand-logo");
+const menuLink = document.querySelectorAll(".menu-link");
 
 
 window.addEventListener("scroll", function () {
@@ -14,3 +15,12 @@ window.addEventListener("scroll", function () {
     brandLogo.classList.remove("hiden");
   }
 });
+
+menuLink.forEach(e => {
+  e.addEventListener("click",()=> {
+    for(let item of menuLink) {
+      item.classList.remove("tick")
+    }
+    e.classList.add("tick")
+  })
+})
